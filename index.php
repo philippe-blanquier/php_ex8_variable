2ab9ab4b4f4f7ca06eadc38f92a82aa6cf4984f7
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:30:52 +0200

    evo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='fr'>
+	<head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 1.8</title>
+
+    </head>
+    <body >
+        <p>
+            <?php
+            $v1=3+4;
+            echo nl2br('3+4='.$v1."\n");
+            $v2=5*20;
+            echo nl2br('5*20='.$v2."\n");
+            $v3=45/5;
+            echo nl2br('45/5='.$v3."\n");
+            ?>
+        <p>
+    </body>
+</html>
